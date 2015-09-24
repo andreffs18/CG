0612946ff35b1aa6d59a7300cb808b7033447b0e
andreffs18 <andreffs18@gmail.com>
2015-09-24 13:04:03 +0100

    saved lab 1

--- Labs/Lab1/lab1/main.cpp
+++ Labs/Lab1/lab1/main.cpp
@@ -2,76 +2,109 @@
 //  CG 2015
 //  Lab #1
 //
-
 #include <iostream>         //
 #include <stdlib.h>         // standard lib
 #include <GLUT/glut.h>      // glut lib
 
 
+void whiteSquare(){
+    // delimit the vertices of a primitive or a group of like primitives
+    glBegin(GL_POLYGON);
+    // define white square
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 0.0f);
+    glVertex3f(1.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, 1.0f, 0.0f);
+    glEnd();
+}
+
+void greenRedSquare(){
+    // start drawing green polygon
+    glBegin(GL_POLYGON);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 0.0f);
+    glVertex3f(-1.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, -1.0f, 0.0f);
+    glEnd();
+
+    // start drawing red polygon
+    glBegin(GL_POLYGON);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(1.0f, 1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 0.0f);
+    glVertex3f(0.0f, -1.0f, 0.0f);
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    glEnd();
+    
+}
+
 //  --------------------------------------------------------------- myDisplay()
 //  Custom display function used when event "glutDisplayFunc" is
 //  executed. This handles the drawing of the scenes
 void myDisplay(void) {
+    // Fundamental steps
+    // #1 Clear all buffers
+    // #2 Draw all lines, dots and polygons
+    // #3 Force drawing
+    
+    // set color to black when buffer get clean
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    // actually cleans buffer (Color buffer)
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glBegin(GL_POLYGON);
-    glVertex3f (-1.0f, -1.0f, 0.0f);
-    glVertex3f (1.0f, -1.0f, 0.0f);
-    glVertex3f (1.0f, 1.0f, 0.0f);
-    glVertex3f (-1.0f, 1.0f, 0.0f);
-    glEnd();
+    //whiteSquare();
+    greenRedSquare();
+    // force the execution of the GL commands
     glFlush();
 }
 
 //  --------------------------------------------------------------- myReshape()
 //  Custom reshape function used when event "glutReshapeFunc" is
-//   executed. This handles the change in screen size
+//  executed. This handles the change in screen size
 
 void myReshape(GLsizei w, GLsizei h) {
+    // define size of the viewport
+    // args: x, y, weight, height
+    // x and y are measure from the bottom left corner of the screen
+    // weight and height are the actual size of the viewport
     glViewport(0, 0, w, h);
+    // changes the pointer for which Matrix we want to work on. GL_PROJECTION
     glMatrixMode(GL_PROJECTION);
+    // puts the Identity Matrix as the top matrix of the stack GL_PROJECTION
     glLoadIdentity();
-    glOrtho(-2.0f, 2.0f, -2.0f, 2.0f, 0.0f, 0.0f);
+    // changes back the pointer to the GL_MODELVIEW
     glMatrixMode(GL_MODELVIEW);
+    // and set's the top matrix of that stack to be the Identity Matrix
     glLoadIdentity();
+       
+    
+    // This call here defines the volume of the projection
+    // args: left, right, bottom, top, nearVal, farVal;
+
+    // Fixing resizing of the window
+    glOrtho(-2.0f, 2.0f, -2.0f, 2.0f, -2.0f, 2.0f);
+    
 }
 
 //  --------------------------------------------------------------- main()
 int main(int argc, char * argv[]) {
 
-    // initialise glut
+    // initialise glut library
     glutInit(&argc, argv);
     // request a RGBA display mode, and we want single buffering
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     // set the initial window size
-    glutInitWindowSize (400, 400);
+    glutInitWindowSize(400, 400);
     // set the initial window position
     glutInitWindowPosition (-1, -1);
-    // create the window
-    glutCreateWindow("Teste");
+    // create the window with properties defined before
+    glutCreateWindow("Lab #1");
+    // set the callback function to use to draw our scene
     glutDisplayFunc(myDisplay);
+    // set the callback function to handle changes in screen size
     glutReshapeFunc(myReshape);
+    // this function runs a forever in a loop to keep the program running
     glutMainLoop();
-    
-    //     glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
-    
-    
-    //     glutInitWindowSize(400,400);
-    
-
-    //     glutInitWindowPosition(-1, -1);
-    
-    
-       //  glutCreateWindow("My first test");
-    
-    // set the function to use to draw our scene
-    //     glutDisplayFunc(myDisplay);
-    
-    // set the function to handle changes in screen size
-
-    
-    // this function runs a while loop to keep the program running.
-       //  glutMainLoop();
     return 0;
 }
